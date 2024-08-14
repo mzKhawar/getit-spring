b0a157f6c3e005130cc753d7cade416fff926472
Mohammad Khawar <mzkhawar775@gmail.com>
2024-08-14 16:23:20 -0500

    initial commit

---
+++ src/main/java/dev/mzkhawar/getit/GetitApplication.java
@@ -0,0 +1,13 @@
+package dev.mzkhawar.getit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mzkhawar/getit/GetitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mzkhawar.getit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
